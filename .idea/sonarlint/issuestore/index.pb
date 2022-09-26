73280749fe003aaca1b6d1289d49f75de1198414
ky-nguyen <kynguyen00710011999@gmail.com>
2022-09-26 15:18:46 +0700

    rendering data to templates

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+@
+requirements.txt,1/9/19359a61ae2446b51b549167b014da2fcf265768
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+E
+DevSearch/settings.py,6/6/66766d8d8982c9243589b11986234a1a75962575
+E
+templates/navbar.html,2/1/2152ecc432cb8e2a5acad94785c9a8c5a8df8cac
+C
+templates/main.html,7/0/70463c664bcf38fa5d46b8f3fd74b75dac46c512
+A
+DevSearch/urls.py,e/5/e5c0fc5415a74ac14df22490180cc25d1f7c3124
--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,39 @@ from django.http import HttpResponse
 # Create your views here.
 
 
+projectList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Build your ecommerce website with Shopify.'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'The striking portfolio websites in this selection demonstrate a unique approach to web design and plenty of personality.'
+    },
+    {
+        'id': '3',
+        'title': 'Social Website',
+        'description': 'Reddit is a social news and entertainment website with tons of sub-communities dedicated to specific interests'
+    },
+]
+
+
 def projects(request):
-    return HttpResponse('Here Are our products')
+    page = 'Hello Ky'
+    number = 10
+    context = {
+        'page': page,
+        'number': number,
+        'projects': projectList
+    }
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return HttpResponse('SINGLE PROJECT' + ' ' + str(pk))
+    projectObj = None
+    for i in projectList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
